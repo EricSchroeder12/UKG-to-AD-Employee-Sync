ceb2ba441581dd5dfc1942b5fac1c360fbc59029
Eric Schroeder <Eric.Schroeder@Truebeck.com>
2023-01-11 14:00:40 -0800

    New UKG Script

---
+++ ADEmployeeIDSync.ps1
@@ -0,0 +1,26 @@
+﻿##AD and UKG ID sync
+## Created by Eric Schroeder and Jarred Hall
+##9/8/2022
+
+
+## Encrypt SQL Login User Password and set variables
+$password = Get-Content "C:\scripts\Password\Password.txt" | ConvertTo-SecureString 
+$credential = New-Object System.Management.Automation.PsCredential("DWSQLsvc",$password)
+
+##SQL Export of UKG ID's to CSV
+Invoke-Sqlcmd -Query "select *  from dbo.ADUserSync" -ServerInstance "DW-PROD-SQL.truebeck.com" -Database "DW_UKG" -Credential $credential |
+Export-Csv -Path "c:\Temp\EmployeeUpload.csv" -NoTypeInformation
+
+##Attache To Active Directory and add new user information
+import-module activedirectory
+$users = import-csv "c:\Temp\EmployeeUpload.csv"
+ForEach ($user in $users) {
+    try {
+        Get-aduser -identity $user.userName | set-aduser -EmployeeID $user.timeclockID -EmployeeNumber $user.employeeNumber
+    }
+    catch { "{0}|{1} " -f $user.userName, $user.employeeNumber  | Out-File -Append -FilePath "C:\scripts\UKG_Sync_Logs\errorlog.txt"
+    }
+}
+
+robocopy C:\scripts\UKG_Sync_Logs \\dw-prod-sql.truebeck.com\UKG_AD_Sync_Logs /MIR /COPY:DAaT /DCOPY:DAT /Z /J /SL /MT:[10] /R:1 /W:10
+
